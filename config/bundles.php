02a62b5a475aaee473f61a2ebe46cedd288252e9
Allan Roberto <allanroberto18@gmail.com>
2018-01-24 19:35:02 -0300

    Including Symfony Components WebProfiler, Twig, Http-Fundamentals Including Bundle FOSJSRoutingBundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,8 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 ];
